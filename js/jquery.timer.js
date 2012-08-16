e603c914fbb5b8a4ae194274cdfb768c0426ebf7
Austin Burns <austin.burns@blitzlocal.com>
2012-08-16 01:54:52 -0400

    Added comments and parameters to set the width and height of the carousel
    - Added comments throughout the whole plugin
    
    - Added the parameters "width" and "height" with set default values of
    "100%"
    
    - Cleaned up the way the css objects looked for better readability
    
    - Added a check to see if the width and height parameters were of type
    "number" and if not a console error is thrown (will create a fix in a
    future commit to automatically strip the number if there is one from
    the strings and use that for the width and height)
    
    - Added a check to see if the height was not "100%" and if not then got
    the height that was set by the paramour and used that instead

--- js/jquery.timer.js
+++ js/jquery.timer.js
@@ -6,36 +6,60 @@
  *	http://www.opensource.org/licenses/mit-license.php
  */
 ;(function($) {
+	//Initialize defaults
 	var defaults = {
 		minutes:	0,
 		seconds:  5,
 		timerColor: '#dc602b',
 		timerBGClass: ".timer-background",
 		timerBGColor: '#96573d',
+		height: "100%",
+		width: "100%",
 		autoplay: 1,
 		loop: 1
 	};
 
 	$.fn.timer = function(settings) {		
 		$.extend(this, {
+			//Add a reset function to be used outside of the plugin in case user would like to reset the plugin
 			reset: function(){
 				img.each(function(i){				
 					if(i === 0)
-						img.eq(i).css({ "position" : "absolute", "display" : "", "opacity" : 1, "z-index" : totalImages - i }).addClass("current");
+						img.eq(i).css({ 
+										"position" : "absolute", 
+										"display"  : "", 
+										"opacity"  : 1, 
+										"width"    : timer.width,
+										"height"   : timer.height,
+										"z-index"  : totalImages - i 
+									  }).addClass("current");
 					else
-						img.eq(i).css({ "position" : "absolute", "display" : "none", "opacity" : 1, "z-index" : (totalImages - i) }).removeClass("current");
+						img.eq(i).css({ 
+										"position" : "absolute", 
+										"display"  : "none", 
+										"opacity"  : 1,
+										"width"    : timer.width,
+										"height"   : timer.height,
+										"z-index"  : totalImages - i
+									  }).removeClass("current");
 				});
 			}
 		});
 		
 		$.extend(this, defaults, settings);
-		
+		//Get the images and find out the total length to be used in the init function
 		var img = $(this).parent().find("img");
 			totalImages = img.length;
-		
+			
+		if(this.height !== "100%" || this.width !== "100%"){
+			if(typeof this.height !== "number" || typeof this.width !== "number")
+				console.error("You can only put a number as a parameter for width and height");
+		}
+
+		//If there are multiple images display the carousel timer and run the plugin, otherwise just display the single image
 		if(totalImages > 1){
 			init(this);
-
+			
 			var timer = this,
 				timerProgress = timer.find(".timer-progress"),
 				pause = timer.find(".pause"),
@@ -47,8 +71,16 @@
 				clicked = 0,
 				selected = 0;
 			
+			/*
+			  Set the setInterval ID so when the mouse hovers over the image we can
+			  use the setInterval ID to destroy the current interval thus causing a "pause" in the timer
+			*/
 			var intID = window.setInterval(function(){progressBar()}, interval);
 			
+			/*
+			  If the outer div (div class carousel) is hovered over the setInterval is destroyed 
+			  when the mouse is not hovering anymore and the pause button wasn't clicked the setInterval continues
+			*/
 			$(outerDiv).hover(
 				function(){
 					window.clearInterval(intID);
@@ -59,6 +91,7 @@
 				}
 			)
 			
+			//If the pause button is clicked set the clicked variable to true and change the bg image to a play button and vice versa
 			$(pause).click(function(){
 				if(clicked === 0){
 					clicked = 1;
@@ -70,6 +103,10 @@
 				}
 			});
 			
+			/*
+			  When one of the picture links is clicked i.e. "PIC / 01" grab the 
+			  index and send it to the switchImages function to change the image to that index
+			*/
 			$(".picture-link").click(function(){
 				var obj = $(this),
 					index = obj.prevAll("a").length;
@@ -78,6 +115,10 @@
 			});
 		}
 		
+		/*
+		  If the percentage is 100 or less increase the width of the timerProgress div by said percentage.
+		  If auto loop is set to true then restart the timer and switch the image
+		*/
 		function progressBar(){
 			if(percent <= 100){
 				$(timerProgress).width(percent + "%");
@@ -93,6 +134,9 @@
 			}
 		};
 		
+		/*
+		  Initialize the images and the timer interface
+		*/
 		function init(timer){
 			var link = "", 
 				current = "", 
@@ -102,19 +146,35 @@
 			img.each(function(i){
 				if(i === 0){
 					current = "current";
-					img.eq(i).css({ "position" : "absolute", "z-index" : totalImages - i }).addClass("current");
+					img.eq(i).css({ 
+									"position" : "absolute", 
+									"z-index"  : totalImages - i,
+									"width"    : timer.width,
+									"height"   : timer.height
+								 }).addClass("current");
 				}
 				else{
-					img.eq(i).css({ "position" : "absolute", "display" : "none", "z-index" : (totalImages - i) });
+					img.eq(i).css({ 
+									"position" : "absolute", 
+									"display"  : "none", 
+									"z-index"  : totalImages - i,
+									"width"    : timer.width,
+									"height"   : timer.height
+								 });
 					current = "";
 				}
 				
-				$("<img/>").attr("src", $(img.eq(i)).attr("src")).load(function() {
-			        if(this.height > tallestHeight)
-						tallestHeight = this.height;
+				if(timer.height !== "100%")
+					jQuery(".carousel").height(+timer.height + 20);
+				else{
+					//Preload the images in memory to get the tallest image height and set the height of the outerDiv to that height
+					$("<img/>").attr("src", $(img.eq(i)).attr("src")).load(function() {
+				        if(this.height > tallestHeight)
+							tallestHeight = this.height;
 						
-					jQuery(".carousel").height(tallestHeight + 20);
-				});
+						jQuery(".carousel").height(tallestHeight + 20);
+					});
+				}
 				
 				link += "<a class='picture-link " + current + "' href='javascript:void(0)' rel='" + i + "'>Pic / <span style='color: " + timer.timerColor + "'>0" + (i + 1) + "</span></a>";
 			});
@@ -125,14 +185,23 @@
 							'</div>');
 		};
 		
+		/*
+		  Takes an an optional parameter of currentIndex, if currentIndex is null then 
+		  switchImages starts from the index of the image that has a the class of "current"
+		  on it.
+		*/
 		function switchImages(currentIndex){
+			
+			//Get the index of the image with class of "current"
 			var startIndex = $(timer).parent().find("img[class*='current']").prevAll("img").length;
 			
 			if(currentIndex === null)
+				//Get the index of the image that will loaded next 
 				currentIndex = $(timer).parent().find("img").eq(startIndex).prevAll("img").length + 1;
 			else
 				selected = 1;
 			
+			//Fade the current image out and remove the class of "current"
 			img.eq(startIndex)
 			   .fadeOut("slow")
 			   .removeClass("current")
@@ -146,14 +215,21 @@
 					currentIndex = currentIndex + i;
 				else if(selected === 1 && i !== 0) 
 					currentIndex++;
-						
+				
+				/*
+				  If we have not reached the last image we want to continue adding to currentIndex,
+				  if we have reached the last image and are not at the end of the for loop we want
+				  to start the count over to go back to the first image
+				*/		
 				if(currentIndex < totalImages)
 					currentIndex = $(timer).parent().find("img").eq(currentIndex).prevAll("img").length;
 				else
 					currentIndex = 0;
 				
+				//Set the images new z-index so the next image can show and the current can hide
 				img.eq(currentIndex).css({ "position" : "absolute", "z-index" : (totalImages - i) });
 				
+				//Show the next image
 				if(i === 0){
 					img.eq(currentIndex)
 					   .fadeIn("slow")
